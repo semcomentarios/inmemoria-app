71fa98da99466428bd067f45c235f46f8d40b7b9
semcomentarios <semcomentarioss@hotmail.com>
2026-05-22 20:11:54 -0300

    Update next.config.js with webpack fallbacks for Pixi.js

--- next.config.js
+++ next.config.js
@@ -4,11 +4,22 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': require('path').resolve(__dirname, './src'),
     };
+    
+    // Adiciona fallbacks para modules do navegador
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+      };
+    }
+    
     return config;
   },
 };
